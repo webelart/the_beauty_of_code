e4952ec924e802739c082d1d842938212b8613ef
Elena Litvinova <hello@webelart.com>
2025-01-12 10:25:56 +0000

    Added leetcode. 199. Binary Tree Right Side View

---
+++ Leetcode/199_binary_tree_right_side_view.js
@@ -0,0 +1,29 @@
+/*
+    Link to the task: https://leetcode.com/problems/binary-tree-right-side-view/description/
+    Decision:
+*/
+
+var rightSideView = function(root) {
+    if (!root) {
+        return [];
+    }
+
+    const stack = [{ node: root, level: 0}];
+    const result = [];
+
+    while(stack.length) {
+        const el = stack.shift();
+
+        result[el.level] = el.node.val
+
+        if (el.node.left) {
+            stack.push({ node: el.node.left, level: el.level + 1});
+        }
+
+        if (el.node.right) {
+            stack.push({ node: el.node.right, level: el.level + 1});
+        }
+    }
+
+    return result;
+};
---
+++ Leetcode/README.md
@@ -0,0 +1,5 @@
+# 199. Binary Tree Right Side View
+
+Link to the task
+English Explanation (YouTube)
+Russian Explanation (YouTube)
